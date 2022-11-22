030bfdc4309cd9bb9c318e97b57164648016f024
Pol Rosinés Pozo <26841927+RosinesP@users.noreply.github.com>
2022-11-22 16:01:27 +0100

    Add files via upload

---
+++ CircleElement.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let active;
+  export let xPos;
+  export let yPos;
+  export let circleSize;
+  export let value;
+
+  let fillCol = "transparent";
+
+  $: if (active) {
+    fillCol = "black";
+    circleSize = 5;
+  } else {
+    fillCol = "transparent";
+    circleSize = 10;
+  }
+</script>
+
+<g>
+  <circle cx={xPos} cy={yPos} r ={circleSize} 
+  fill={fillCol} on:mouseover on:mouseout />
+
+  <text x={xPos+10} y={yPos+10} fill = {fillCol}>{Math.round(value)}</text>
+
+
+</g>
